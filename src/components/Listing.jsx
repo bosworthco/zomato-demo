dd9504fde9370c2f606096dddfa1a6785212f39b
James Bosworth <jsbosworth@gmail.com>
2020-11-09 21:11:14 +1300

    Accessibility enhancements

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -5,7 +5,7 @@ import Background from 'assets/urban-hero.webp';
 class Listing extends Component {
   render() {
     return (
-      <div className="Display-listing u-md-width2of3">
+      <div className="Display-listing u-md-width2of3" tabindex="0">
         <div className="Listing">
 
           <div className="Listing-image" style={{backgroundImage: `url(${Background})`}} />
@@ -21,17 +21,17 @@ class Listing extends Component {
           </ul>
 
           <section className="Listing-cuisines">
-            <h3 className="h6 u-mB-sm">Cuisines</h3>
+            <h2 className="h6 u-mB-sm">Cuisines</h2>
             <p className="h5">Coffee and Tea, Cafe Food</p>
           </section>
 
           <section className="Listing-contact">
-            <h3 className="h6 u-mB-sm">Phone number</h3>
+            <h2 className="h6 u-mB-sm">Phone number</h2>
             <p className="h5">+64 3 546 7861</p>
           </section>
 
           <section className="Listing-hours">
-            <h3 className="h6 u-mB-sm">Opening Hours</h3>
+            <h2 className="h6 u-mB-sm">Opening Hours</h2>
             <p className="h5">4pm – 11pm (Today) <span className="Listing-hours-status" data-open="true">Open now</span></p>
           </section>
         </div>
